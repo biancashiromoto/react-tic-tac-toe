dde6e2b4236ec8d896c1624b626a4d4a630aa64f
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-04-26 21:24:12 -0300

    Create board, start adding gameplay functions

---
+++ src/components/GridUnit/GridUnit.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './GridUnit.css';
+
+class GridUnit extends Component {
+  render() {
+    const { index, grids, onClick, playerSymbol } = this.props;
+    // console.log(index);
+    return (
+      <div
+        className="grid-unit"
+        onClick={ ({ target }) => {
+          target.innerHTML = playerSymbol;
+          grids[index] = playerSymbol;
+          onClick(target, index);
+        }}
+      >
+      </div>
+    )
+  }
+}
+
+export default GridUnit
