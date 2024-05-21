863631d76b91d41c191a61cd179189d7d60d11d2
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-21 20:24:36 -0300

    refactor:  Implement TS and change component PlayerDisplay to functional

---
+++ src/components/PlayerDisplay/PlayerDisplay.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import player1Symbol from '../../assets/img/o-item.png';
+import player2Symbol from '../../assets/img/x-item.png';
+import './PlayerDisplay.css';
+import { PlayerDisplayPropsInterface } from '../../interfaces/Interfaces';
+
+// class PlayerDisplay extends Component {
+//   render() {
+//     const {
+//       isPlayer1Turn,
+//     } = this.props;
+//     return (
+//       <div
+//         className="player-display-container"
+//       >
+//         <img
+//           src={ isPlayer1Turn ? player1Symbol : player2Symbol }
+//           alt={ isPlayer1Turn ? 'O' : 'X' }
+//           className="player-img-display"
+//         />
+//         <h3>{isPlayer1Turn ? 'Player 1' : 'Player 2'}</h3>
+//       </div>
+//     )
+//   }
+// }
+
+// export default PlayerDisplay;
+
+const PlayerDisplay: React.FC<PlayerDisplayPropsInterface> = ({ isPlayer1Turn }: PlayerDisplayPropsInterface) => {
+  return (
+    <div
+        className="player-display-container"
+      >
+        <img
+          src={ isPlayer1Turn ? player1Symbol : player2Symbol }
+          alt={ isPlayer1Turn ? 'O' : 'X' }
+          className="player-img-display"
+        />
+        <h3>{isPlayer1Turn ? 'Player 1' : 'Player 2'}</h3>
+      </div>
+  )
+}
+
+export default PlayerDisplay;
