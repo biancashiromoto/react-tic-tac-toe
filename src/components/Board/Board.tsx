ec77a3492ed14896c5c18d19f8be8c1e1829e6bb
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-26 15:41:09 -0300

    refactor: Update component Board to pass down prop isGameOver to component Square

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from '../Square/Square';
 import './Board.css';
 import { BoardPropsInterface } from "../../interfaces/Interfaces";
 
-const Board: React.FC<BoardPropsInterface> = ({ grids, playerSymbol, checkMove }) => {
-  const [board, setBoard] = useState<string[]>(['', '', '', '', '', '', '', '', '']);
+const Board: React.FC<BoardPropsInterface> = ({ grids, playerSymbol, checkMove, isGameOver }) => {
+  const board: string[] = new Array(9).fill("");
 
   return (
     <div className="board">
       {board.map((_item, index) => (
         <Square
+          isGameOver={isGameOver}
           key={index}
           index={index}
           grids={grids}
