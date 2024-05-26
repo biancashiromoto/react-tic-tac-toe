e5640577614c4f7916d483c2ad9a8034eb94da25
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-26 16:02:35 -0300

    refactor: Use _grids from Utils as mapped array

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Square from '../Square/Square';
 import './Board.css';
 import { BoardPropsInterface } from "../../interfaces/Interfaces";
+import { Utils } from '../../utils/utils';
 
 const Board: React.FC<BoardPropsInterface> = ({ grids, playerSymbol, checkMove, isGameOver }) => {
-  const board: string[] = new Array(9).fill("");
+  const { _grids } = new Utils();
 
   return (
     <div className="board">
-      {board.map((_item, index) => (
+      {_grids.map((_item, index) => (
         <Square
           isGameOver={isGameOver}
           key={index}
