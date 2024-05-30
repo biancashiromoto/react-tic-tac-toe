54c2565289e907147fba756d6c2170c99a8d10bb
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-30 15:47:12 -0300

    chore: Create interface ContextProps

--- src/interfaces/Interfaces.ts
+++ src/interfaces/Interfaces.ts
@@ -39,3 +39,18 @@ export interface MessageContainerPropsInterface {
   gameOverMessage: string;
   isTie: boolean;
 }
+
+export interface ContextProps {
+  grids: string[];
+  setGrids: React.Dispatch<React.SetStateAction<string[]>>;
+  isGameOver: boolean;
+  setIsGameOver: React.Dispatch<React.SetStateAction<boolean>>;
+  isPlayer1Turn: boolean;
+  setIsPlayer1Turn: React.Dispatch<React.SetStateAction<boolean>>;
+  playerSymbol: string;
+  setPlayerSymbol: React.Dispatch<React.SetStateAction<string>>;
+  gameOverMessage: string;
+  setGameOverMessage: React.Dispatch<React.SetStateAction<string>>;
+  isTie: boolean;
+  setIsTie: React.Dispatch<React.SetStateAction<boolean>>;
+}
