ce135c4478f98100e2d2627099915a642f6142ac
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-31 19:19:30 -0300

    chore: Update SquarePropsInterface, BoardPropsInterface and ButtonPropsInterface to not take grids

--- src/interfaces/Interfaces.ts
+++ src/interfaces/Interfaces.ts
@@ -1,23 +1,11 @@
-export interface GameStateInterface {
-  grids: string[];
-  isPlayer1Turn: boolean;
-  playerSymbol: string;
-  isGameOver: boolean;
-  gameOverMessage: string;
-  disabled: boolean;
-  isTie: boolean;
-}
-
 export interface SquarePropsInterface {
   index: number;
-  grids: string[];
   playerSymbol: string;
   checkMove: (grids: string[]) => void;
   isGameOver?: boolean;
 }
 
 export interface BoardPropsInterface {
-  grids: string[];
   playerSymbol: string;
   checkMove: (grids: string[]) => void;
   isGameOver?: boolean;
@@ -26,7 +14,6 @@ export interface BoardPropsInterface {
 
 export interface ButtonPropsInterface {
   buttonValue: string;
-  grids: string[];
   restartGame: () => void;
   isGameOver?: boolean;
 }
