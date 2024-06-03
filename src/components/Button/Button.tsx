43b4e8a11fada8e9fde05a77e362a901f463b29d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-06-03 20:07:00 -0300

    test: Add data-testid to component Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,12 +3,13 @@ import './Button.css';
 import { ButtonPropsInterface } from '../../interfaces/Interfaces';
 import { context } from '../../context/context';
 
-const Button: React.FC<ButtonPropsInterface> = ({ label, onClick, className }) => {
+const Button: React.FC<ButtonPropsInterface> = ({ label, onClick, className, dataTestId }) => {
   const { cells } = useContext(context);
 
   return (
     <button
       className={className}
+      data-testid={dataTestId}
       disabled={cells.every(cell => cell === "")}
       type="button"
       onClick={() => onClick()}
