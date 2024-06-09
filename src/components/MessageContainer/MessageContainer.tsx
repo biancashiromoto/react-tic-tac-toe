517314cee792b6377560a5880429ae2a74eb53f5
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-06-09 18:51:17 -0300

    refactor: Implement usePlayerState to manage isPlayer1Turn state

--- src/components/MessageContainer/MessageContainer.tsx
+++ src/components/MessageContainer/MessageContainer.tsx
@@ -2,19 +2,20 @@ import React, { useContext } from 'react';
 import '../../App.css';
 import './MessageContainer.css';
 import { context } from '../../context/context';
+import usePlayerState from '../../hooks/usePlayerState';
 
 const MessageContainer: React.FC = () => {
   const {
-    isPlayer1Turn,
     gameOverMessage,
     isTie
   } = useContext(context);
+  const { isPlayer1Turn } = usePlayerState();
 
   const backgroundColor = (isTie: boolean): string => {
     if (isTie) {
       return "tie";
-    }
-    return `${isPlayer1Turn ? "player2" : "player1"}`;
+    }    
+    return `${isPlayer1Turn ? "player1" : "player2"}`;
   }
   
   return (
