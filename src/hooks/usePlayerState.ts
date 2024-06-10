35ef5bbc036938d93049c4efc0ce3809d656c1c1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-06-10 19:56:55 -0300

    feat: Create function switchPlayer

--- src/hooks/usePlayerState.ts
+++ src/hooks/usePlayerState.ts
@@ -1,7 +1,13 @@
 import { useContext } from "react"
 import { context } from "../context/context"
+import { Utils } from "../utils/utils";
 
 export const usePlayerState = () => {
+  const {
+    _player1Symbol,
+    _player2Symbol,
+  } = new Utils();
+
   const { 
     isPlayer1Turn,
     setIsPlayer1Turn,
@@ -9,10 +15,16 @@ export const usePlayerState = () => {
     setPlayerSymbol
   } = useContext(context);
 
+  const switchPlayer = () => {
+    setIsPlayer1Turn(prevState => !prevState);
+    setPlayerSymbol(prevState => prevState === _player1Symbol ? _player2Symbol : _player1Symbol);
+  };
+
   return { 
     isPlayer1Turn,
     setIsPlayer1Turn,
     playerSymbol,
     setPlayerSymbol,
+    switchPlayer
    };
 }
