3568592a0d3c759d29f2b4917ec69e2d833f0dc7
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-06-10 20:34:37 -0300

    feat: Create function updateCells in hook useCellState

--- src/hooks/useCellState.ts
+++ src/hooks/useCellState.ts
@@ -1,11 +1,24 @@
 import { useContext } from "react"
 import { context } from "../context/context"
+import { usePlayerState } from "./usePlayerState";
+import { Utils } from "../utils/utils";
 
 export const useCellState = () => {
   const { 
     cells,
     setCells,
   } = useContext(context);
+  const { playerSymbol } = usePlayerState();
+  const { handleMove } = new Utils();
 
-  return { cells, setCells };
+  const updateCells = (index: number) => {
+    handleMove(index, cells, playerSymbol);
+    setCells(prevCells => {
+      const newCells = [...prevCells];
+      newCells[index] = playerSymbol;
+      return newCells;
+    });
+  }
+
+  return { cells, setCells, updateCells };
 }
