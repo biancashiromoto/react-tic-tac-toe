56716fea07d710dcad0909bbe208f418df691fef
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-06-22 12:46:42 -0300

    feat: Implement button to toggle between muted and unmuted modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
+import { useContext } from 'react';
 import Board from './components/Board/Board';
 import Button from './components/Button/Button';
 import MessageContainer from './components/MessageContainer/MessageContainer';
 import PlayerDisplay from './components/PlayerDisplay/PlayerDisplay';
 import { useCellState, useGameState } from './hooks';
+import { context } from './context/context';
 
 const App = () => {
   const { cells } = useCellState();
   const { isGameOver, resetGame } = useGameState();
+  const { isMuted, setIsMuted } = useContext(context);
 
   return (
     <div
       className="App"
       data-testid="app"
     >
+      <Button
+        label={isMuted ? "MUTED" : "NOT MUTED"}
+        onClick={() => setIsMuted(prevState => !prevState)}
+        type="button"
+      />
       <h1>Tic-tac-toe</h1>
       {!isGameOver && <PlayerDisplay />}
       <Board />
