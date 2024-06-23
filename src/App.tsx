39ca55d4c288295e046208145439b9333e08db30
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-06-23 20:09:16 -0300

    style: Fix MessageContainer positioning

--- src/App.tsx
+++ src/App.tsx
@@ -30,17 +30,19 @@ const App = () => {
       </Button.Root>
       <h1>Tic-tac-toe</h1>
       <div className="game__container">
-        {!isGameOver && <PlayerDisplay />}
-        <Board />
+        <PlayerDisplay />
+        <div className="board-message__container">
+          <Board />
+          {isGameOver && <MessageContainer />}
+        </div>
         <Button.Root
           className={isGameOver ? 'pulse restart-game-button' : 'restart-game-button'}
           onClick={() => resetGame()}
           dataTestId="restart-game-button"
           disabled={cells.every(cell => cell === "")}
-        >
+          >
           <Button.Label label="Restart" />
         </Button.Root>
-        {isGameOver && <MessageContainer />}
       </div>
     </div>
   )
