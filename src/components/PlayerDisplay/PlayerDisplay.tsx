5e9551c91c67295b9c93a86bd618e32c52e9f51a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-07 19:35:24 -0300

    style: Implement Tailwind to style component PlayerDisplay

--- src/components/PlayerDisplay/PlayerDisplay.tsx
+++ src/components/PlayerDisplay/PlayerDisplay.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import player1Symbol from '../../assets/img/o-item.png';
 import player2Symbol from '../../assets/img/x-item.png';
-import './PlayerDisplay.css';
 import { Utils } from '../../utils/utils';
 import { usePlayerState } from '../../hooks';
 
@@ -15,15 +14,15 @@ const PlayerDisplay: React.FC = () => {
 
   return (
     <div
-        className="player-display-container"
+        className="player-display-container flex gap-4 items-center justify-center"
         data-testid="player-display-container"
       >
         <img
           src={ isPlayer1Turn ? player1Symbol : player2Symbol }
           alt={ isPlayer1Turn ? _player1Symbol : _player2Symbol }
-          className="player-img-display"
+          className="player-img-display w-[35px]"
         />
-        <h3>{isPlayer1Turn ? 'Player 1' : 'Player 2'}</h3>
+        <h3 className="font-semibold text-3xl font-londrina-solid">{isPlayer1Turn ? 'Player 1' : 'Player 2'}</h3>
       </div>
   )
 }
