f2f4fbef002e49cb1680d5a384099be50f5469cb
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-07 17:13:04 -0300

    style: Implement styling with Tailwindng with Tailwind

--- src/components/MessageContainer/MessageContainer.tsx
+++ src/components/MessageContainer/MessageContainer.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import '../../App.css';
-import './MessageContainer.css';
 import { useGameState } from '../../hooks';
 
 const MessageContainer: React.FC = () => {
@@ -8,10 +6,10 @@ const MessageContainer: React.FC = () => {
   
   return (
     <div
-        className={`message-container ${addClassName()}`}
+        className={`message-container flex flex-col items-center justify-center rounded-3xl fixed top-52 right-[50%] translate-x-2/4 transition-all h-44 w-44 hover:scale-110 ${addClassName()}`}
         data-testid="message-container"
       >
-        <h2>{gameOverMessage}</h2>
+        <h2 className="font-londrina-solid font-semibold text-4xl">{gameOverMessage}</h2>
       </div>
   )
 }
