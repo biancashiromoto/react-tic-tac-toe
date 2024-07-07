1c7ae2d1ccdd58de79a5e3ec29a04661e918a868
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-07 19:58:44 -0300

    refactor: Pass isPlayer1Turn to addClassName

--- src/components/MessageContainer/MessageContainer.tsx
+++ src/components/MessageContainer/MessageContainer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useGameState } from '../../hooks';
+import { useGameState, usePlayerState } from '../../hooks';
 
 const MessageContainer: React.FC = () => {
   const { gameOverMessage, addClassName } = useGameState();
+  const { isPlayer1Turn } = usePlayerState();
   
   return (
     <div
-        className={`message-container flex flex-col items-center justify-center rounded-3xl top-56 right-[50%] translate-x-2/4 absolute transition-all h-40 w-44 hover:scale-110 text-center ${addClassName()}`}
+        className={`message-container flex flex-col items-center justify-center rounded-3xl top-56 right-[50%] translate-x-2/4 absolute transition-all h-40 w-44 hover:scale-110 text-center ${addClassName(isPlayer1Turn)}`}
         data-testid="message-container"
       >
         <h2 className="font-londrina-solid font-semibold text-4xl">{gameOverMessage}</h2>
