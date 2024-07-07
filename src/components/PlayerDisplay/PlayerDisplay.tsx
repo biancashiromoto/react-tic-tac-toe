7a3ef730b2803a1f37f2c6e46cec29c03827f6ae
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-07 20:58:06 -0300

    refactor: Use variable TEXT to manage application's texts

--- src/components/PlayerDisplay/PlayerDisplay.tsx
+++ src/components/PlayerDisplay/PlayerDisplay.tsx
@@ -3,6 +3,7 @@ import player1Symbol from '../../assets/img/o-item.png';
 import player2Symbol from '../../assets/img/x-item.png';
 import { Utils } from '../../utils/utils';
 import { usePlayerState } from '../../hooks';
+import { TEXT } from '../../__text';
 
 const PlayerDisplay: React.FC = () => {
   const { isPlayer1Turn } = usePlayerState();
@@ -22,7 +23,7 @@ const PlayerDisplay: React.FC = () => {
           alt={ isPlayer1Turn ? _player1Symbol : _player2Symbol }
           className="player-img-display w-[35px]"
         />
-        <h3 className="font-semibold text-3xl font-londrina-solid">{isPlayer1Turn ? 'Player 1' : 'Player 2'}</h3>
+        <h3 className="font-semibold text-3xl font-londrina-solid">{isPlayer1Turn ? TEXT.player1 : TEXT.player2}</h3>
       </div>
   )
 }
