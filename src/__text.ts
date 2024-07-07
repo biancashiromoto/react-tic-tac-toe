3f357bdd397854bcaa54116a894374bab42f6272
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-07 20:56:25 -0300

    feat: Create file text

---
+++ src/__text.ts
@@ -0,0 +1,13 @@
+interface TextInterface {
+  title: string;
+  player1: string;
+  player2: string;
+  restartButton: string;
+}
+
+export const TEXT: TextInterface = {
+  title: "tic-tac-toe",
+  player1: "Player 1",
+  player2: "Player 2",
+  restartButton: "Restart",
+}
