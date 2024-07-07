0addad4141f5ab30717c8a6d61bdbc4a31bb102f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-07 20:56:44 -0300

    feat: Implement interface for COLORS

--- src/__variables.ts
+++ src/__variables.ts
@@ -1,4 +1,14 @@
-export const COLORS = {
+interface ColorsInterface {
+  background: string;
+  hover: string;
+  restartButton: {
+    enabled: string;
+    disabled: string;
+  };
+  white: string;
+}
+
+export const COLORS: ColorsInterface = {
   background: "bg-sky-950",
   hover: "hover:bg-sky-900",
   restartButton: {
