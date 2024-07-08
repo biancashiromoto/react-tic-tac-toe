fab4ca041617fdff4c27a6010c5d2f40ebe58ee3
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-07 21:52:46 -0300

    tests: Implement data-testids and texts from variables

--- src/components/Cell/Cell.spec.tsx
+++ src/components/Cell/Cell.spec.tsx
@@ -3,6 +3,7 @@ import Provider from "../../context/Provider";
 import player1Symbol from "../../assets/img/o-item.png";
 import player2Symbol from "../../assets/img/x-item.png";
 import Cell from "./Cell";
+import { TEST_IDS } from "../../__variables";
 
 describe("Component Cell", () => {
   beforeEach(() => {
@@ -14,21 +15,21 @@ describe("Component Cell", () => {
   });
   
   it("should be correctly rendered", () => {
-    expect(screen.getByTestId("cell")).toBeInTheDocument();
+    expect(screen.getByTestId(TEST_IDS.cell)).toBeInTheDocument();
   });
   
   it("should correctly handle click", () => {
-    expect(screen.getByTestId("cell")).not.toHaveClass("disabled");
-    fireEvent.click(screen.getByTestId("cell"));
-    expect(screen.getByTestId("cell")).toHaveClass("disabled");
-    expect(screen.getByTestId("cell").children[0]).toHaveAttribute("src", player1Symbol);
+    expect(screen.getByTestId(TEST_IDS.cell)).not.toHaveClass("disabled");
+    fireEvent.click(screen.getByTestId(TEST_IDS.cell));
+    expect(screen.getByTestId(TEST_IDS.cell)).toHaveClass("disabled");
+    expect(screen.getByTestId(TEST_IDS.cell).children[0]).toHaveAttribute("src", player1Symbol);
   });
   
   it("should not change if it is already disabled", () => {
-    fireEvent.click(screen.getByTestId("cell"));
-    expect(screen.getByTestId("cell").children[0]).toHaveAttribute("src", player1Symbol);
-    fireEvent.click(screen.getByTestId("cell"));
-    expect(screen.getByTestId("cell").children[0]).toHaveAttribute("src", player1Symbol);
-    expect(screen.getByTestId("cell").children[0]).not.toHaveAttribute("src", player2Symbol);
+    fireEvent.click(screen.getByTestId(TEST_IDS.cell));
+    expect(screen.getByTestId(TEST_IDS.cell).children[0]).toHaveAttribute("src", player1Symbol);
+    fireEvent.click(screen.getByTestId(TEST_IDS.cell));
+    expect(screen.getByTestId(TEST_IDS.cell).children[0]).toHaveAttribute("src", player1Symbol);
+    expect(screen.getByTestId(TEST_IDS.cell).children[0]).not.toHaveAttribute("src", player2Symbol);
   });
 });
