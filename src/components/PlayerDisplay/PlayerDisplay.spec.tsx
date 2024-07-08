af63f87f62fbf68f07eaf6efb1f9109bb7e0675d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-07 21:49:08 -0300

    tests: Implement data-testids and texts from variables

--- src/components/PlayerDisplay/PlayerDisplay.spec.tsx
+++ src/components/PlayerDisplay/PlayerDisplay.spec.tsx
@@ -3,6 +3,8 @@ import App from "../../App";
 import Provider from "../../context/Provider";
 import player1Symbol from '../../assets/img/o-item.png';
 import player2Symbol from '../../assets/img/x-item.png';
+import { TEST_IDS } from "../../__variables";
+import { TEXT } from "../../__text";
 
 
 let cells: Element[];
@@ -13,46 +15,46 @@ describe("Component PlayerDisplay", () => {
         <App />
       </Provider>
     );
-    cells = getAllByTestId("cell");
+    cells = getAllByTestId(TEST_IDS.cell);
   });
 
   it("should correctly display the current player", () => {
-    const container = screen.getByTestId("player-display-container");
-    expect(container).toHaveTextContent(/player 1/i);
-    expect(container).not.toHaveTextContent(/player 2/i);
+    const container = screen.getByTestId(TEST_IDS.playerDisplay);
+    expect(container).toHaveTextContent(TEXT.player1);
+    expect(container).not.toHaveTextContent(TEXT.player2);
     expect(container.firstChild).toHaveAttribute("src", player1Symbol);
     
     fireEvent.click(cells[0]);
-    expect(container).toHaveTextContent(/player 2/i);
-    expect(container).not.toHaveTextContent(/player 1/i);
+    expect(container).toHaveTextContent(TEXT.player2);
+    expect(container).not.toHaveTextContent(TEXT.player1);
     expect(container.firstChild).toHaveAttribute("src", player2Symbol);
     
     fireEvent.click(cells[1]);
-    expect(container).toHaveTextContent(/player 1/i);
-    expect(container).not.toHaveTextContent(/player 2/i);
+    expect(container).toHaveTextContent(TEXT.player1);
+    expect(container).not.toHaveTextContent(TEXT.player2);
     expect(container.firstChild).toHaveAttribute("src", player1Symbol);
   });
 
   it("should set the current player as player 1 after player 1 wins", () => {
-    const container = screen.getByTestId("player-display-container");
+    const container = screen.getByTestId(TEST_IDS.playerDisplay);
     fireEvent.click(cells[0]);
     fireEvent.click(cells[3]);
     fireEvent.click(cells[1]);
     fireEvent.click(cells[4]);
     fireEvent.click(cells[2]);
-    fireEvent.click(screen.getByTestId("restart-game-button"));
-    expect(container).toHaveTextContent(/player 1/i);
+    fireEvent.click(screen.getByTestId(TEST_IDS.buttons.restartGame));
+    expect(container).toHaveTextContent(TEXT.player1);
   });
 
   it("should set the current player as player 1 after player 2 wins", () => {
-    const container = screen.getByTestId("player-display-container");
+    const container = screen.getByTestId(TEST_IDS.playerDisplay);
     fireEvent.click(cells[3]);
     fireEvent.click(cells[6]);
     fireEvent.click(cells[4]);
     fireEvent.click(cells[7]);
     fireEvent.click(cells[0]);
     fireEvent.click(cells[8]);
-    fireEvent.click(screen.getByTestId("restart-game-button"));
-    expect(container).toHaveTextContent(/player 1/i);
+    fireEvent.click(screen.getByTestId(TEST_IDS.buttons.restartGame));
+    expect(container).toHaveTextContent(TEXT.player1);
   });
 });
